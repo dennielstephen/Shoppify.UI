946bf386994048bd89cb505de0df3e66e197e3d2
Stephen Mordeno <dennielstephen@gmail.com>
2023-05-06 10:10:09 +0800

    Fixed Tailwind Configuration
    Fixed Tailwind Configuration and updated the cheatsheet.tsx file to test the integration of Material UI Component and Tailwind styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+// import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { UserConfig } from 'vite';
+
+const config: UserConfig = {
+  optimizeDeps: {
+    exclude: ['@mui/icons-material'],
+    include: ['@mui/material', '@emotion/react', '@emotion/styled'],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+};
+
+export default config;
